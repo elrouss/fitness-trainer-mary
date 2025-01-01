a6c677d97400b026c1a58909747c588a7c546a7b
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2025-01-01 23:29:21 +0300

    FTM-29: block modal scrollbar on phones

--- frontend/src/components/dialog/components/dialog/dialog.ts
+++ frontend/src/components/dialog/components/dialog/dialog.ts
@@ -1,47 +1,48 @@
 export class Dialog {
-    root: HTMLDialogElement;
+    root: HTMLDivElement;
 
     closeButton: HTMLButtonElement;
 
     constructor (className: string) {
-        this.root = document.querySelector(`.${className}`) as HTMLDialogElement;
+        this.root = document.querySelector(`.${className}`) as HTMLDivElement;
 
         this.closeButton = this.root.querySelector('.dialog__close-button') as HTMLButtonElement;
 
         this.close = this.close.bind(this);
 
-        this.root.addEventListener('close', this.setScrollBack);
         this.root.addEventListener('click', this.closeOnBackdrop);
 
         this.closeButton.addEventListener('click', this.close);
     }
 
-    private setScrollBack = () => {
-        document.body.classList.remove('scroll-lock');
-    };
-
     private closeOnBackdrop = ({ target, currentTarget }: Event) => {
         const dialog = currentTarget;
         const isClickedOnBackdrop = target === dialog;
 
-        if (!(dialog instanceof HTMLDialogElement) || !isClickedOnBackdrop) {
+        if (!(dialog instanceof HTMLDivElement) || !isClickedOnBackdrop) {
             return;
         }
 
         this.close();
     };
 
-    open () {
-        this.root.showModal();
+    private closeOnEscape = (event: KeyboardEvent) => {
+        if (event.key === 'Escape') {
+            this.close();
+        }
+    };
 
+    open () {
+        this.root.classList.add('dialog_open');
         document.body.classList.add('scroll-lock');
+
+        document.addEventListener('keydown', this.closeOnEscape);
     }
 
     close () {
-        this.root.close();
+        this.root.classList.remove('dialog_open');
+        document.body.classList.remove('scroll-lock');
 
-        // this.root?.removeEventListener('close', this.setScrollBack);
-        // this.root?.removeEventListener('click', this.closeOnBackdrop);
-        // this.closeButton?.removeEventListener('click', this.close);
+        document.removeEventListener('keydown', this.closeOnEscape);
     }
 }
